fbebf21d900b5dae7130563863323a31d56f8b6e
Pavol Loffay <ploffay@redhat.com>
2017-04-13 11:31:38 +0200

    Add Jaeger OpenTracing tracer

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ ENV AB_ENABLED off
 ENV AB_JOLOKIA_AUTH_OPENSHIFT true
 ENV JAVA_OPTIONS -Xmx256m -Djava.security.egd=file:///dev/./urandom
 ENV ZIPKIN_SERVER_URL http://zipkin
+ENV JAEGER_SERVER_URL jaeger-all-in-one
+ENV TRACING_SYSTEM none
 
 EXPOSE 8080
 
--- src/main/java/com/redhat/developers/msa/ola/TracingConfiguration.java
+++ src/main/java/com/redhat/developers/msa/ola/TracingConfiguration.java
@@ -1,10 +1,19 @@
 package com.redhat.developers.msa.ola;
 
 import java.util.Collections;
+import java.util.regex.Pattern;
 
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.uber.jaeger.metrics.Metrics;
+import com.uber.jaeger.metrics.NullStatsReporter;
+import com.uber.jaeger.metrics.StatsFactoryImpl;
+import com.uber.jaeger.reporters.RemoteReporter;
+import com.uber.jaeger.samplers.ProbabilisticSampler;
+import com.uber.jaeger.senders.Sender;
+import com.uber.jaeger.senders.UDPSender;
 
 import brave.opentracing.BraveTracer;
 import feign.Logger;
@@ -15,6 +24,7 @@ import feign.opentracing.TracingClient;
 import feign.opentracing.hystrix.TracingConcurrencyStrategy;
 import io.opentracing.NoopTracerFactory;
 import io.opentracing.Tracer;
+import io.opentracing.contrib.spring.web.autoconfig.WebTracingConfiguration;
 import zipkin.Span;
 import zipkin.reporter.AsyncReporter;
 import zipkin.reporter.Reporter;
@@ -25,19 +35,46 @@ import zipkin.reporter.urlconnection.URLConnectionSender;
  */
 @Configuration
 public class TracingConfiguration {
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(TracingConfiguration.class);
+    private static final String SERVICE_NAME = "ola";
 
     @Bean
     public Tracer tracer() {
-        String zipkinServerUrl = System.getenv("ZIPKIN_SERVER_URL");
-        if (zipkinServerUrl == null) {
-            return NoopTracerFactory.create();
+        String tracingSystem = System.getenv("TRACING_SYSTEM");
+        if ("zipkin".equals(tracingSystem)) {
+            log.info("Using Zipkin tracer");
+            return zipkinTracer(System.getenv("ZIPKIN_SERVER_URL"));
+        } else if ("jaeger".equals(tracingSystem)) {
+            log.info("Using Jaeger tracer");
+            return jaegerTracer(System.getenv("JAEGER_SERVER_URL"));
         }
 
-        System.out.println("Using Zipkin tracer");
-        Reporter<Span> reporter = AsyncReporter.builder(URLConnectionSender.create(zipkinServerUrl + "/api/v1/spans"))
+        log.info("Using Noop tracer");
+        return NoopTracerFactory.create();
+    }
+
+    private Tracer zipkinTracer(String url) {
+        Reporter<Span> reporter = AsyncReporter.builder(URLConnectionSender.create(url + "/api/v1/spans"))
                 .build();
-        brave.Tracer braveTracer = brave.Tracer.newBuilder().localServiceName("ola").reporter(reporter).build();
+        brave.Tracer braveTracer = brave.Tracer.newBuilder().localServiceName(SERVICE_NAME).reporter(reporter).build();
         return BraveTracer.wrap(braveTracer);
+    }
+
+    private Tracer jaegerTracer(String url) {
+        Sender sender = new UDPSender(url, 0, 0);
+        return new com.uber.jaeger.Tracer.Builder(SERVICE_NAME,
+                new RemoteReporter(sender, 100, 50,
+                        new Metrics(new StatsFactoryImpl(new NullStatsReporter()))),
+                new ProbabilisticSampler(1.0))
+                .build();
+    }
+
+
+    @Bean
+    public WebTracingConfiguration webTracingConfiguration() {
+        return WebTracingConfiguration.builder()
+                .withSkipPattern(Pattern.compile("/api/health"))
+                .build();
     }
 
     /**
